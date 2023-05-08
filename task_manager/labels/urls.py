f15f501e8e1498cdafd0b7a8b2af73ae3b401c8a
Timofey <tima_yakovishin@mail.ru>
2023-05-08 14:49:05 +0300

    [refactor] fix grammar mistake x2

--- task_manager/labels/urls.py
+++ task_manager/labels/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexViews.as_view(), name='labels'),
     path('create/', views.LabelCreateView.as_view(), name='create_label'),
-    path('<int:id>/update', views.LabelUpdateView.as_view(), name='update_label'),
-    path('<int:id>/delete', views.LabelDeleteView.as_view(), name='delete_label'),
+    path('<int:id>/update/', views.LabelUpdateView.as_view(), name='update_label'),
+    path('<int:id>/delete/', views.LabelDeleteView.as_view(), name='delete_label'),
 ]
