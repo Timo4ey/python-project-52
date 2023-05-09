f23e3a725f2e9175999bf13cacbc004459755d9d
Timofey <tima_yakovishin@mail.ru>
2023-05-09 14:54:36 +0300

    [refactor] update tasks

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -10,14 +10,14 @@ from django.utils.translation import gettext_lazy as _
 
 class F(django_filters.FilterSet):
     STATUSES_CHOICE = TaskStatus.objects.all().values_list('id', 'name')
-    PERFORMER_CHOICE = User.objects.all()
+    PERFORMER_CHOICE = [(x.id, x.get_full_name()) for x in User.objects.all()]
     STATUSES_TAGS = Label.objects.all().values_list('id', 'name')
 
     creator = django_filters.BooleanFilter(label=_("Только свои задачи"), method='my_custom_filter',
                                            widget=forms.CheckboxInput)
-
     status = django_filters.ChoiceFilter(label=_("Статус"), choices=STATUSES_CHOICE)
     labels = django_filters.ChoiceFilter(label=_("Метка"), choices=STATUSES_TAGS)
+    executor = django_filters.ChoiceFilter(label=_("Исполнитель"), choices=PERFORMER_CHOICE)
 
     class Meta:
         model = Tasks
