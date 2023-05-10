b54ccce3e6e764eb81ba24300daae6a44830f798
Timofey <tima_yakovishin@mail.ru>
2023-05-10 08:57:48 +0300

    [refactor] delete a custom executor from filter

--- task_manager/tasks/filters.py
+++ task_manager/tasks/filters.py
@@ -1,5 +1,4 @@
 import django_filters
-from django.contrib.auth.models import User
 
 from .models import Tasks
 from task_manager.task_status.models import TaskStatus
@@ -10,14 +9,12 @@ from django.utils.translation import gettext_lazy as _
 
 class F(django_filters.FilterSet):
     STATUSES_CHOICE = TaskStatus.objects.all().values_list('id', 'name')
-    PERFORMER_CHOICE = [(x.id, x.get_full_name()) for x in User.objects.all()]
     STATUSES_TAGS = Label.objects.all().values_list('id', 'name')
 
     creator = django_filters.BooleanFilter(label=_("Только свои задачи"), method='my_custom_filter',
                                            widget=forms.CheckboxInput)
     status = django_filters.ChoiceFilter(label=_("Статус"), choices=STATUSES_CHOICE)
     labels = django_filters.ChoiceFilter(label=_("Метка"), choices=STATUSES_TAGS)
-    executor = django_filters.ChoiceFilter(label=_("Исполнитель"), choices=PERFORMER_CHOICE)
 
     class Meta:
         model = Tasks
